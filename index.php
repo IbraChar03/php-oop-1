c72a4b4457f91145bb21ac6be5be5deb6d8dbc86
Ibrahim <ibrachar68@gmail.com>
2023-01-24 16:34:30 +0100

    inizio bonus 1

--- index.php
+++ index.php
@@ -6,27 +6,40 @@ class GenreType
     {
         $this->genre = $genre;
     }
+
 }
 class Movie
 {
     public $name;
     public $dateRelease;
-
-    public $genreType;
-    public function __construct($name, $dateRelease, $genreType)
+    public $genresType;
+    public function __construct($name, $dateRelease, $genresType)
     {
         $this->name = $name;
         $this->dateRelease = $dateRelease;
-        $this->genreType = $genreType;
+        $this->genresType = $genresType;
     }
+    public function getGenres()
+    {
+        $str = "";
+        foreach ($this as $genresType => $genreType) {
+            $str .= $genreType->genre;
+
+        }
+        return $str;
+    }
+
     public function getHtml()
     {
-        return "<h1>" . $this->name . "(" . $this->dateRelease . ")" . $this->genreType->genre . "</h1>";
+        return "<h1>" . $this->name . "(" . $this->dateRelease . ")" . $this->getGenres() . "</h1>";
+        // $this->genreType->genre
     }
 }
 
-$genre1 = new GenreType("thriller");
+$genre1 = new GenreType("Thriller");
+$genre2 = new GenreType("Horror");
+$genres1 = [$genre1, $genre2];
 
-$movie1 = new Movie("Avengers", "2022-05-03", $genre1);
+$movie1 = new Movie("Avengers", "2022-05-03", $genres1);
 
 echo $movie1->getHtml();
