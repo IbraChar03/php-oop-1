ea3291c30c646097e476a7601ea6b9e1c66aa5e0
Ibrahim <ibrachar68@gmail.com>
2023-01-24 15:44:44 +0100

    creazione class genre

--- index.php
+++ index.php
@@ -1,20 +1,32 @@
 <?php
+class GenreType
+{
+    public $genre;
+    public function __construct($genre)
+    {
+        $this->genre = $genre;
+    }
+}
 class Movie
 {
     public $name;
     public $dateRelease;
-    public function __construct($name, $dateRelease)
+
+    public $genreType;
+    public function __construct($name, $dateRelease, $genreType)
     {
         $this->name = $name;
-
         $this->dateRelease = $dateRelease;
-
+        $this->genreType = $genreType;
     }
     public function getHtml()
     {
-        return "<h1>" . $this->name . "(" . $this->dateRelease . ")" . "</h1>";
+        return "<h1>" . $this->name . "(" . $this->dateRelease . ")" . $this->genreType->genre . "</h1>";
     }
 }
-$movie1 = new Movie("Avengers", "2022-05-03");
+
+$genre1 = new GenreType("thriller");
+
+$movie1 = new Movie("Avengers", "2022-05-03", $genre1);
 
 echo $movie1->getHtml();
